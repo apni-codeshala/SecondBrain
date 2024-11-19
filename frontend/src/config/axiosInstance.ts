8600682eb64818f83b781e3a7a28db77df0f01a4
apni-codeshala <abhishekpatel171019@gmail.com>
2024-11-19 06:55:12 +0000

    Minor fix for deployment

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://backendsecond-brain.vercel.app//api/v1";
+const BASE_URL = "https://backendsecond-brain.vercel.app/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
