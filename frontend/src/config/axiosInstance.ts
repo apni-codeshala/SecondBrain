3291256465819982421dba80b76716c6c3527d99
apni-codeshala <abhishekpatel171019@gmail.com>
2024-11-19 00:38:50 +0000

    Frontend fix

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://backendsecond-brain.vercel.app//api/v1/";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
