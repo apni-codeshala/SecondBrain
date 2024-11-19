ff26fbfaa8c37a77a8a3dc56b11eb6ada805528e
apni-codeshala <abhishekpatel171019@gmail.com>
2024-11-19 07:00:41 +0000

    Minor fix for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,13 +9,7 @@ import apiRoutes from "./routes/index";
 const app = express();
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "https://secondbrain-kappa.vercel.app",
-    methods: "GET,POST,PUT,DELETE,OPTIONS, PATCH",
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
